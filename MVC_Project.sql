b1832cb880f185fa1a1351d59fbcbc28f30f6aae
SnehalS26 <136054327+SnehalS26@users.noreply.github.com>
2023-09-09 22:40:46 +0530

    Add files via upload

--- MVC_Project.sql
+++ MVC_Project.sql
@@ -40,8 +40,72 @@ create table Employee
 eid int primary key identity(1,1),
 ename varchar(40),
 salary int,
-did int
+did int,
 constraint did_fk foreign key(did) references Department(did)
 )
+alter table Employee add imageurl varchar(40)
+drop table Employe
+
+create table Employee
+(
+id int primary key identity(1,1),
+name varchar(30),
+salary numeric(12,2),
+imageurl varchar(40),
+did int
+)
+
 select * from Department
 select * from Employee
+
+create table Category
+(
+cid int primary key identity(1,1),
+cname varchar(40)
+)
+
+create table Product
+(
+id int primary key identity(1,1),
+name varchar(30),
+price numeric(12,2),
+imageurl varchar(40),
+cid int
+)
+select * from Category
+select * from Product
+
+-------------------------------------------------------------
+
+create table Users
+(
+id int primary key identity(1,1),
+firstName varchar(50),
+lastName varchar(50),
+email varchar(200),
+password varchar(50),
+phone varchar(50),
+gender varchar(50),
+city varchar(50),
+state varchar(50),
+)
+alter table Users add roleid int foreign key(roleid) references Roles(roleid)
+select * from Users
+update Users set roleid=1 where id=1
+
+create table Roles
+(
+roleid int primary key,
+rolename varchar(40)
+)
+select * from Roles
+insert into Roles values(1,'admin')
+insert into Roles values(2,'User')
+
+create table ContactUs
+(
+name varchar(50),
+email varchar(50),
+message varchar(300)
+)
+select * from ContactUs
